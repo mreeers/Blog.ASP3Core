6566e8b854e9e4ba6a39da168476a00a797c287d
Александр Ефанов <verpl01@yandex.ru>
2020-09-10 22:01:56 +0500

    MVC configuration

--- Blog.UI/Startup.cs
+++ Blog.UI/Startup.cs
@@ -16,6 +16,7 @@ namespace Blog.UI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,13 +29,20 @@ namespace Blog.UI
 
             app.UseRouting();
 
+            //app.UseMvcWithDefaultRoute();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
             });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
