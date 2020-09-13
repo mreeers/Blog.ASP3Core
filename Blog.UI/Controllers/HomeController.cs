6e40aceb66d3e78da545d683550eb297b30dd32b
Александр Ефанов <verpl01@yandex.ru>
2020-09-13 16:34:57 +0500

    Add repository

--- Blog.UI/Controllers/HomeController.cs
+++ Blog.UI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.UI.Controllers.Repository.Interface;
+using Blog.UI.Data;
+using Blog.UI.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +11,13 @@ namespace Blog.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRepository _repository;
+
+        public HomeController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +26,22 @@ namespace Blog.UI.Controllers
         public IActionResult Post()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Post post)
+        {
+            _repository.AddPost(post);
+            if (await _repository.SaveChangesAsync())
+                return RedirectToAction("Index");
+            else
+                return View(post);
         }
     }
 }
---
+++ Blog.UI/Controllers/Repository/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Blog.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.UI.Controllers.Repository.Interface
+{
+    public interface IRepository
+    {
+        Post GetPost(int id);
+        List<Post> GetAllPosts(int id);
+        void AddPost(Post post);
+        void RemovePost(int id);
+        void UpdatePost(Post post);
+        Task<bool> SaveChangesAsync();
+    }
+}
