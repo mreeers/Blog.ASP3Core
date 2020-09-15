726aa819f8a1fd831b5551ebef0f8b3f7d87988d
Александр Ефанов <verpl01@yandex.ru>
2020-09-15 22:21:48 +0500

    Add style

--- Blog.UI/ViewModels/LoginViewModel.cs
+++ Blog.UI/ViewModels/LoginViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Blog.UI.ViewModels
     public class LoginViewModel
     {
         public string UserName { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
